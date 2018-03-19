43f23206350b28f7e72078b013e646720e32ebba
Vu Ngo <vungo2004@gmail.com>
2018-03-19 12:49:09 -0700

    change basecomp calc, move it to wrapper

--- baseComposition.py
+++ baseComposition.py
@@ -10,7 +10,9 @@ from sys import argv
 
 def main():
 	backgroundfile=argv[1]
-	output=argv[2]
+	alphabetfile = argv[2] # alphabet, a squence of alphabets that are used, separated by commas. Example: A,G,C,T
+	output = argv[3]
+
 
 	seqs=open(backgroundfile).read().split('\n')
 	total=float(seqs[0].strip().split('\t')[1])
@@ -19,27 +21,27 @@ def main():
 	totalalphacounts={}
 	kmerlen=len(seqs[0].strip().split('\t')[0])
 	for line in seqs:
-	    tmp=line.strip().split("\t")
-	    if len(tmp)<2:
-	        #print tmp
-	        continue
-	    kmercount=int(tmp[1])
-	    alphacounts={}
-	    for char in tmp[0]:
-	        if char not in alphacounts:
-	            alphacounts[char]=1
-	        else:
-	            alphacounts[char]+=1
-	    for char in alphacounts:
-	        if char not in totalalphacounts:
-	            totalalphacounts[char]=alphacounts[char]*kmercount
-	        else:
-	            totalalphacounts[char]+=alphacounts[char]*kmercount
+		tmp=line.strip().split("\t")
+		if len(tmp)<2:
+			#print tmp
+			continue
+		kmercount=int(tmp[1])
+		alphacounts={}
+		for char in tmp[0]:
+			if char not in alphacounts:
+				alphacounts[char]=1
+			else:
+				alphacounts[char]+=1
+		for char in alphacounts:
+			if char not in totalalphacounts:
+				totalalphacounts[char]=alphacounts[char]*kmercount
+			else:
+				totalalphacounts[char]+=alphacounts[char]*kmercount
 
 	outfile=open(output,'w')
 	for char in totalalphacounts:
-	    line=char+'\t'+str(float(totalalphacounts[char])/total/kmerlen)
-	    outfile.write(line+'\n')
+		line=char+'\t'+str(float(totalalphacounts[char])/total/kmerlen)
+		outfile.write(line+'\n')
 	return
 
 
